7c907a5177b4bc418e612a7c73bfa4d0ea212639
freelook <limon1991@inbox.ru>
2014-04-09 21:18:19 +0300

    deploy to heroky

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
-	stylus = require('stylus');
+	stylus = require('stylus')
+mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str, path){
+function compile(str, path) {
 	return stylus(str).set('filename', path);
 }
 
@@ -16,17 +17,38 @@ app.configure(function() {
 	app.use(express.bodyParser());
 	app.use(stylus.middleware(
 		{
-			src: __dirname +  '/public',
+			src: __dirname + '/public',
 			compile: compile
 		}
 	));
 	app.use(express.static(__dirname + '/public'));
 });
 
-app.get('*', function(req, res){
-	res.render('index');
+//mongoose.connect('mongodb://localhost/mean');
+mongoose.connect('mongodb://mean:mean@ds045897.mongolab.com:45897/mean');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+	console.log('db opened');
 });
 
-var port = 4000;
+var messageSchema = mongoose.Schema({message: String});
+var Messages = mongoose.model('Messages', messageSchema);
+var mongoMsg;
+Messages.findOne().exec(function(err, msgDoc) {
+	mongoMsg = msgDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+	res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+	res.render('index', {
+		mongoMsg: mongoMsg
+	});
+});
+
+var port = process.env.PORT || 4000;
 app.listen(port);
 console.log('Listening on port ' + port + ' ...');
