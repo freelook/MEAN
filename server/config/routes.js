ce7c6696de25f4775aeef709316c02ba0079ff95
freelook <freelook@mail.ua>
2014-04-23 12:29:09 +0300

    locale demo

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,7 +6,12 @@ module.exports = function(app){
 		res.sendfile('app/' + req.params.partialPath);
 	});
 	app.get('*', function(req, res) {
-		res.render('index');
-		console.log(req.locale);
+		if(req.locale && req.locale.code){
+		var ln = req.locale.code.split('_')[0].toLowerCase();
+		ln = (ln === 'ru' || ln === 'en')? ln : 'en';
+		}
+		var locale = require('../locale/' + ln);
+		res.render('index', locale);
+		console.log();
 	});
 };
