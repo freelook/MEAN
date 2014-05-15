87b529ddf0369092e1e50d9e65dcf3271e9e7699
freelook <freelook@mail.ua>
2014-05-15 16:47:21 +0300

    vk

--- app/core/controllers/main.js
+++ app/core/controllers/main.js
@@ -1,9 +1,25 @@
-define(['app'], function(app) {
+define(['app', 'core/js/vk'], function(app, vk) {
 	'use strict';
 
 	app.register.controller('main', function($scope) {
 		$scope.myVar = "Hello Angular";
-		console.log('Hello Angular');
 	});
 
+	VK.init({apiId: 3520312, onlyWidgets: true});
+	VK.Widgets.Like("vk", {type: "button", height: 24});
+
+	try {
+		if (VK && VK.Observer && VK.Observer.subscribe) {
+			VK.Observer.subscribe('widgets.like.liked', function() {
+				// todo liked
+				console.log('liked');
+			});
+			VK.Observer.subscribe('widgets.like.unliked', function() {
+				// todo unliked
+				console.log('unliked');
+			});
+		}
+	} catch (e) {
+		console.error(e.message);
+	}
 });
