3df744ed85a0bc486f6b8f5ccf4b26d231258ed8
freelook <freelook@mail.ua>
2014-06-03 17:10:55 +0300

    ng-disable

--- app/core/controllers/mainCtrl.js
+++ app/core/controllers/mainCtrl.js
@@ -2,8 +2,10 @@ define(['app', 'core/js/vk'], function(app, vk) {
 	'use strict';
 
 	app.register.controller('mainCtrl', function($scope, $http, $window, socket) {
+		console.info('mainCtrl');
 		// TODO: get date from server
 		var date = (new Date()).getTime();
+		$scope.inButtonDisable = false;
 		$scope.lcz = lcz;
 
 		$scope.signin = function() {
@@ -13,7 +15,6 @@ define(['app', 'core/js/vk'], function(app, vk) {
 				}
 			});
 		};
-
 		VK.init({apiId: 3520312, onlyWidgets: true});
 		var loc = location.href.replace(/#.*$/, '') + '?time=' + date;
 		VK.Widgets.Like("vk", {type: "button", height: 24, pageUrl: loc});
@@ -22,13 +23,14 @@ define(['app', 'core/js/vk'], function(app, vk) {
 			if (VK && VK.Observer && VK.Observer.subscribe) {
 				VK.Observer.subscribe('widgets.like.liked', function() {
 					console.log('in');
+					$scope.inButtonDisable = true;
 					$scope.signin();
 					socket.emit('msg', {
 						message: 'in'
 					});
-					//$scope.$emit('goBack');
 				});
 				VK.Observer.subscribe('widgets.like.unliked', function() {
+					$scope.inButtonDisable = false;
 					console.log('un');
 				});
 			}
