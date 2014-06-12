17b6ba9c1c447bb523c8868afa6e31839aa2bda6
freelook <freelook@mail.ua>
2014-06-12 20:54:55 +0300

    app bone

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,7 +1,7 @@
 var http = require('http');
-//1401868310976
+
 exports.authenticate = function(req, res, next) {
-	http.get('http://api.vk.com/method/likes.getList?type=sitepage&owner_id=3520312&extended=1&page_url=dev.freelook.info/vk?time=' +
+	http.get('http://api.vk.com/method/likes.getList?type=sitepage&owner_id=3520312&extended=1&page_url=dev.freelook.info/vk?name=vk&time=' +
 			req.query.time,
 		function(VKRes) {
 			var data;
